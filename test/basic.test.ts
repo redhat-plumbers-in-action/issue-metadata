00390d25ee89759958695d5c601d1e2878a31f5e
Jan Macku <jamacku@redhat.com>
2023-12-12 10:34:07 +0100

    test: use vitest instead of node-fetch

---
+++ test/basic.test.ts
@@ -0,0 +1,73 @@
+import { afterEach, beforeEach, describe, expect, test, vi } from 'vitest';
+
+import MetadataController from '../src/index';
+
+const mocks = vi.hoisted(() => {
+  return {
+    request: vi.fn(),
+  };
+});
+
+vi.mock('@octokit/request', async () => {
+  const actual = await vi.importActual('@octokit/request');
+  const request = mocks.request;
+  return { ...(actual as any), request };
+});
+
+const owner = 'redhat-plumbers-in-action';
+const repo = 'issue-metadata';
+const issueNumber = 2;
+const token = '0000000000000000000000000000000000000001';
+const metadata = new MetadataController('metadata_id', {
+  owner,
+  repo,
+  headers: {
+    authorization: `token ${token}`,
+  },
+});
+
+describe('Integration tests ', () => {
+  beforeEach(async () => {
+    // Mock GitHub API
+    vi.mocked(mocks.request)
+      // GET /repos/{owner}/{repo}/issues/{issue_number}
+      .mockResolvedValueOnce({
+        status: 200,
+        data: {
+          body: "I'm having a problem with this. Please HELP!!! :sos: ",
+        },
+      })
+      // PATCH /repos/{owner}/{repo}/issues/{issue_number}
+      .mockResolvedValueOnce({
+        status: 200,
+        data: {
+          body: `I'm having a problem with this. Please HELP!!! :sos: \n\n<!-- metadata_id = {"foo":"bar"} -->`,
+        },
+      })
+      // GET /repos/{owner}/{repo}/issues/{issue_number}
+      .mockResolvedValueOnce({
+        status: 200,
+        data: {
+          body: `I'm having a problem with this. Please HELP!!! :sos: \n\n<!-- metadata_id = {"foo":"bar"} -->`,
+        },
+      });
+  });
+
+  afterEach(async () => {
+    vi.restoreAllMocks();
+  });
+
+  test('Set and Get Metadata using Controller', async () => {
+    expect(metadata).toBeInstanceOf(MetadataController);
+
+    expect(await metadata.setMetadata(issueNumber, 'foo', 'bar'))
+      .toMatchInlineSnapshot(`
+      {
+        "foo": "bar",
+      }
+    `);
+
+    const data = metadata.getMetadata(issueNumber, 'foo');
+    expect(await data).toMatchInlineSnapshot('"bar"');
+  });
+});
