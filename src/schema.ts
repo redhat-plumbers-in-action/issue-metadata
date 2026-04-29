f0f366762f660c0d93d936dc1cac99e2bd92d8d3
Jan Macku <jamacku@redhat.com>
2026-04-29 16:31:20 +0200

    deps: update dependencies

--- src/schema.ts
+++ src/schema.ts
@@ -12,26 +12,23 @@ export const metadataSchema = z.object({
 
 export type Metadata = z.infer<typeof metadataSchema>;
 
-export const requestDetailsSchema = z.object(
-  {
-    owner: z.string().min(1),
-    repo: z.string().min(1),
-    headers: z.object({
-      authorization: z.string().min(1),
-    }),
-  },
-  {
-    required_error: `Required settings wasn't provided: {owner: string, repo: string}`,
-  }
-);
+export const requestDetailsSchema = z.object({
+  owner: z.string().min(1),
+  repo: z.string().min(1),
+  headers: z.object({
+    authorization: z.string().min(1),
+  }),
+});
 
 export type RequestDetails = z.infer<typeof requestDetailsSchema>;
 
 export const metadataObjectSchema = z.record(
-  z.union([z.string(), z.array(z.string()), z.array(z.record(z.any()))]),
-  {
-    required_error: `Metadata doesn't have an expected structure: {key: string, value: string | string[] | object[]}`,
-  }
+  z.string(),
+  z.union([
+    z.string(),
+    z.array(z.string()),
+    z.array(z.record(z.string(), z.any())),
+  ])
 );
 
 export type MetadataObject = z.infer<typeof metadataObjectSchema>;
